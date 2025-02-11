76664063068f56fa1d762a75218b0d061660008c
NeverEverFelix <felixmftc@icloud.com>
2025-02-11 15:02:10 -0500

    holy shit please work

--- my-app/vite.config.js
+++ my-app/vite.config.js
@@ -2,6 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
+
 export default defineConfig({
-  plugins: [react()],
-})
+  root: ".", // Ensure Vite looks in the project root
+  build: {
+    outDir: "dist",
+  },
+});
+
