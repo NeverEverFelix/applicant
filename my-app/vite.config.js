a82e0204c2c3e7baedc9546f1bc9af87a6c64245
NeverEverFelix <felixmftc@icloud.com>
2025-02-11 15:05:50 -0500

    omfg

--- my-app/vite.config.js
+++ my-app/vite.config.js
@@ -1,12 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-
-// https://vite.dev/config/
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  plugins: [react()],
   root: ".", // Ensure Vite looks in the project root
   build: {
     outDir: "dist",
-  },
+    rollupOptions: {
+      input: "index.html" // Ensure Vite knows where index.html is
+    }
+  }
 });
 
