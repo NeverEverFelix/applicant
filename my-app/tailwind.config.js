e76213f2144f30257dd647fea9bd6b5b45e2a7ca
NeverEverFelix <felixmftc@icloud.com>
2025-02-15 17:27:42 -0500

    UI build setup

---
+++ my-app/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{js,jsx,ts,tsx}"],
+    theme: {
+      extend: {
+        colors: {
+          primary: "#1E40AF",
+          secondary: "#1E3A8A",
+        },
+      },
+    },
+    plugins: [],
+  };
+  
