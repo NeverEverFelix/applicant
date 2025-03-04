a6f27a402480ae4c493865eaccc6f3d439401e11
NeverEverFelix <felixmftc@icloud.com>
2025-03-03 19:08:02 -0500

    re-fixing

--- my-app/src/main.jsx
+++ my-app/src/main.jsx
@@ -6,8 +6,10 @@ import React from "react";
 
 
 createRoot(document.getElementById('root')).render(
+    <StrictMode>
     <App />
-)
+    </StrictMode>
+);
 // require('dotenv').config();  // Load the .env file
 
 // const chatgptApiKey = process.env.CHATGPT_API_KEY;
